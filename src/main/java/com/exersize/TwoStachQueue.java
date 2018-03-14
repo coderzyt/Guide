e5059209fe54a75bd3d178ee4f1a6341d783e13a
clay <zyt5282@outlook.com>
2018-03-14 23:21:55 +0800

    exercise

--- src/main/java/com/exersize/TwoStachQueue.java
+++ src/main/java/com/exersize/TwoStachQueue.java
@@ -7,9 +7,40 @@ import java.util.Stack;
  */
 public class TwoStachQueue {
 
-    private Stack<Integer> stack;
+    private Stack<Integer> stackPush;
+
+    private Stack<Integer> stackPop;
 
     public TwoStachQueue() {
-        this.stack = new Stack<Integer>();
+        this.stackPush = new Stack<Integer>();
+        this.stackPop = new Stack<Integer>();
+    }
+
+    public void add(int value) {
+        stackPush.push(value);
+    }
+
+    public int poll() {
+        if (stackPop.empty() && stackPush.empty()) {
+            throw new RuntimeException();
+        }
+        else if (stackPop.empty()) {
+            while (!stackPush.empty()) {
+                stackPop.push(stackPush.pop());
+            }
+        }
+        return stackPop.pop();
+    }
+
+    public int peek() {
+        if (stackPop.empty() && stackPush.empty()) {
+            throw new RuntimeException();
+        }
+        else if (stackPop.empty()) {
+            while (!stackPush.empty()) {
+                stackPop.push(stackPush.pop());
+            }
+        }
+        return stackPop.peek();
     }
 }
