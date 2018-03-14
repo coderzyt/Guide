4a6aee7488f324e970476cb2215a0bb8139a6f37
zyt528 <zyt528@outlook.com>
2018-03-14 22:50:11 +0800

    exercise

---
+++ src/main/java/com/exersize/TwoStachQueue.java
@@ -0,0 +1,15 @@
+package com.exersize;
+
+import java.util.Stack;
+
+/**
+ * TwoStachQueue
+ */
+public class TwoStachQueue {
+
+    private Stack<Integer> stack;
+
+    public TwoStachQueue() {
+        this.stack = new Stack<Integer>();
+    }
+}
