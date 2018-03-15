6c719b378fe311f7b94ac2f4757e3284c4234f10
clay.zhao <clay.zhao@corp.to8to.com>
2018-03-15 16:55:04 +0800

    exercise

--- src/main/java/com/clay/ReverseStack.java
+++ src/main/java/com/clay/ReverseStack.java
@@ -7,28 +7,34 @@ import java.util.Stack;
  */
 public class ReverseStack {
 
-    private Stack<Integer> stack;
-
-    private Stack<Integer> reverseStack;
-
-    public ReverseStack() {
-        this.stack = new Stack<Integer>();
-        this.reverseStack = new Stack<Integer>();
+    public static int getLastAndRemoveElement(Stack<Integer> stack) {
+        int result = stack.pop();
+        if (stack.empty()) {
+            return result;
+        }
+        int last = getLastAndRemoveElement(stack);
+        stack.push(result);
+        return last;
     }
 
-    public void reverse() {
-        while (!stack.empty()) {
-            reverseStack.push(stack.pop());
+    public static void reverse(Stack<Integer> stack) {
+        if (stack.empty()) {
+            return;
         }
+        int last = getLastAndRemoveElement(stack);
+        reverse(stack);
+        stack.push(last);
     }
 
-    public int reverse2() {
-        if (!stack.empty()) {
-            return stack.pop();
-        }
-        else {
-            int result = reverse2();
-            sta
-        }
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        System.out.println(stack);
+        reverse(stack);
+        System.out.println(stack);
     }
 }
