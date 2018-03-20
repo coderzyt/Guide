2a7d0c6b4a0442866493e24f835bf585f41e9406
clay <zyt5282@outlook.com>
2018-03-20 22:33:03 +0800

    exercise

---
+++ src/main/java/com/clay/Address.java
@@ -0,0 +1,45 @@
+package main.java.com.clay;
+
+/**
+ * Address
+ */
+public class Address {
+    String address;
+
+    String city;
+
+    String state;
+
+    final String ADDRESS_DATA_FILE = "Address.txt";
+
+    public Address(String add, String cty, String st) {
+        this.address = add;
+        this.city = cty;
+        this.st = st;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public boolean isValid() {
+        if (getState().trim().length() == 2) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean save() {
+        FileUtil fileUtil = new FileUtil();
+        String dataLine = getAddress() + ", " + getCity() + ", " + getState();
+        return fileUtil.writeToFile(ADDRESS_DATA_FILE, true, true);
+    }
+}
